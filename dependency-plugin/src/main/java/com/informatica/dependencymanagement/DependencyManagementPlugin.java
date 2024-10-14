32a80a4c3f3fece43eb857a3269deeee0cfbfa39
RohanSoni97 <rohansoni4641@gmail.com>
2024-10-14 14:52:14 +0530

    Changelist : * Adding common dependencies.gradle

---
+++ dependency-plugin/src/main/java/com/informatica/dependencymanagement/DependencyManagementPlugin.java
@@ -0,0 +1,9 @@
+package com.informatica.dependencymanagement;
+
+
+public class DependencyManagementPlugin {
+
+  public static void main(String[] args) {
+    System.out.println("DependencyManagementPlugin ");
+  }
+}
